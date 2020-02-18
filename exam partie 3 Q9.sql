99bed8dd86bf80758f5a49e11900fc9611323df5
sacha <57749656+sacha174@users.noreply.github.com>
2020-02-18 23:09:10 +0100

    Update exam partie 3 Q9.sql

--- exam partie 3 Q9.sql
+++ exam partie 3 Q9.sql
@@ -1,3 +1,3 @@
-SELECT AVG(ABS(TIMESTAMPDIFF(year, '2014-01-01', Date_de_naissance))) as age_moyen_H FROM elus WHERE sexe = 'F';
+SELECT AVG(ABS(TIMESTAMPDIFF(year, '2014-01-01', Date_de_naissance))) as age_moyen_H FROM elus WHERE sexe = 'M';
 
-#il y a une erreur car date de naissance n'a pas de "_" mais si on ne le met pas il prend date comme une commande.
+#meme code utiliser pour femme juste ne changant le M et  le age moyen.
